f7e07ab5d375a7d6b5c623b958fc998abd1d8dc4
Ryan Raposo <raposo.ryan@gmail.com>
2020-06-10 10:10:12 -0400

    update instald to be modular

--- tools/nstald/nstald.py
+++ tools/nstald/nstald.py
@@ -1,8 +1,8 @@
-import shutil, os, pathlib
+import shutil, os, pathlib, sys
 
 
 def get_mod_dir():
-    MOD_DIR_SEGMENT = "Program Files (x86)\Steam\steamapps\common\Don't Starve Together\mods"
+    MOD_DIR_SEGMENT = r"Program Files (x86)\Steam\steamapps\common\Don't Starve Together\mods"
     cwd = str(pathlib.Path.cwd())
     current_drive = cwd[:3]
     mod_dir = current_drive + MOD_DIR_SEGMENT
@@ -10,10 +10,11 @@ def get_mod_dir():
         mod_dir = input("Path to mods not not found at [" + mod_dir + "] Please enter the correct path: ")
     return mod_dir
 
-if __name__ == "__main__":
+
+def get_installed_mods() -> dict:
     dst_mods = get_mod_dir()
     mod_dir_list = os.listdir(dst_mods)
-
+    mods = {}
     for member in mod_dir_list:
         path = os.path.join(dst_mods, member)
         if os.path.isdir(path) and ".zip" not in path:
@@ -24,10 +25,20 @@ if __name__ == "__main__":
                         if "name=" in line or "name =" in line:
                             workshop_id = os.path.basename(member)
                             name = line.split("=")[1]
-                            print(name + workshop_id + "\n")
+                            name = (name.replace('"', "")).strip()
+                            workshop_id = workshop_id.strip()
+                            mods[name] = workshop_id
                             break
+    return mods                        
+
+
+if __name__ == "__main__":
+    mods = get_installed_mods()
+    for name, workshop_id in mods.items():
+        print(name + '\n' + workshop_id + '\n')
                         
     input("> Press any key to exit: ")
+    sys.exit()
 
                     
 
